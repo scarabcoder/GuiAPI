0533b4efde7d1241b8ba1a73a35488fcd0e4af06
Nicholas Harris (ScarabCoder) <scarabcoder@gmail.com>
2018-03-22 09:04:26 -0700

    Refactored GuiBuilder functions, made item() and onClick() extension functions return the type it's operated on.

--- src/main/kotlin/com/scarabcoder/guiapi/GuiBuilder.kt
+++ src/main/kotlin/com/scarabcoder/guiapi/GuiBuilder.kt
@@ -3,7 +3,6 @@ package com.scarabcoder.guiapi
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
-import java.util.Arrays.asList
 
 fun gui(title: String, player: Player, size: Int, init: GuiScreen.() -> Unit): GuiScreen {
     val g = GuiScreen(title, player, size)
@@ -18,18 +17,6 @@ fun GuiScreen.button(x: Int, y: Int, init: GuiButton.() -> Unit = {}): GuiScreen
     return this
 }
 
-fun GuiButton.onClick(action: (Player) -> Unit): GuiButton {
-    this.action = action
-    return this
-}
-
-fun GuiComponent.item(type: Material, init: ItemBuilder.() -> Unit): GuiComponent {
-    val itemBuilder = ItemBuilder(type)
-    itemBuilder.init()
-    this.item = itemBuilder
-    return this
-}
-
 
 fun GuiScreen.box(fromX: Int, fromY: Int, toX: Int, toY: Int, init: GuiBox.() -> Unit = {}): GuiScreen {
     val b = GuiBox(fromX, fromY, toX, toY)
@@ -38,14 +25,14 @@ fun GuiScreen.box(fromX: Int, fromY: Int, toX: Int, toY: Int, init: GuiBox.() ->
     return this
 }
 
-fun showTo(player: Player) {
-    gui("My Amazing UI", player, 9) {
-        button(0, 0) {
-            item(Material.COMPASS) {
-                name = ""
-                lore = asList("")
-            }
-        }
-        box(0, 0, 5, 5)
-    }
+fun <T: GuiComponent>T.onClick(action: (Player) -> Unit): T {
+    this.action = action
+    return this
+}
+
+fun <T: GuiComponent>T.item(type: Material, init: ItemBuilder.() -> Unit): T {
+    val itemBuilder = ItemBuilder(type)
+    itemBuilder.init()
+    this.item = itemBuilder
+    return this
 }
